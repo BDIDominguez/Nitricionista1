489f9be6185a0bc1f8390f7606b7ed4708379820
Matias <matiasmatteoni@hotmail.com>
2023-10-06 20:45:20 -0300

    olvide agregar entidad a mi controlador jaja

--- src/controladores/ControladorDieta.java
+++ src/controladores/ControladorDieta.java
@@ -14,7 +14,7 @@ import java.sql.Connection;
 import javax.swing.JOptionPane;
 import vistas.VistaDieta;
 import vistas.VistaPantallaPrincipal;
-
+import entidades.EntidadDieta;
 /**
  *
  * @author Matias
