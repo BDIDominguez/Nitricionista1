9da2c0fd07eca7e31d8eaa7e49ab814cda3e0415
gimenezdiegoruben <gimenezdiegoruben@gmail.com>
2023-10-06 21:02:55 -0300

    quité código de Controlador DietaComida
    Voy a reformular el código ya que se me mezclaba como lo iba a mostraros en la vista (ya  que usa Comida y Dieta) pero ya tengo la idea más clara

--- src/controladores/ControladorDieta_Comida.java
+++ src/controladores/ControladorDieta_Comida.java
@@ -5,12 +5,15 @@ package controladores;
  */
 import datas.DataDieta_Comida;
 import entidades.EntidadDieta_Comida;
+import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.FocusEvent;
 import java.awt.event.FocusListener;
 import vistas.VistaDieta_Comida;
 import java.sql.SQLException;
 import java.util.List;
 import javax.swing.JOptionPane;
+import javax.swing.event.ListSelectionEvent;
 import javax.swing.event.ListSelectionListener;
 import javax.swing.table.DefaultTableModel;
 import vistas.VistaPantallaPrincipal;
@@ -34,75 +37,26 @@ public class ControladorDieta_Comida implements ActionListener, FocusListener, L
        
     }
 
-//    private void editarDietaComida() {
-//        int idDietaComida = Integer.parseInt(vista.getTxtIdDietaComida().getText());
-//        int idDieta = Integer.parseInt(vista.getTxtIdDieta().getText());
-//        int idComida = Integer.parseInt(vista.getTxtIdComida().getText());
-//        double porcion = Double.parseDouble(vista.getTxtPorcion().getText());
-//        EntidadDieta_Comida.HorarioComida horario = (EntidadDieta_Comida.HorarioComida) vista.getCbHorario().getSelectedItem();
-//
-//        EntidadDieta_Comida dietaComida = new EntidadDieta_Comida(idDietaComida, idDieta, idComida, porcion, horario);
-//
-//        try {
-//            dataDietaComida.editarDietaComida(dietaComida);
-//            vista.mostrarMensaje("Relación Dieta-Comida editada correctamente.");
-//        } catch (SQLException ex) {
-//            JOptionPane.showMessageDialog(vista, "Error al editar la relación Dieta-Comida: " + ex.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
-//        }
-//    }
-//
-//    private void eliminarDietaComida() {
-//        int idDietaComida = Integer.parseInt(vista.getTxtIdDietaComida().getText());
-//
-//        try {
-//            dataDietaComida.eliminarDietaComida(idDietaComida);
-//            vista.mostrarMensaje("Relación Dieta-Comida eliminada correctamente.");
-//        } catch (SQLException ex) {
-//            JOptionPane.showMessageDialog(vista, "Error al eliminar la relación Dieta-Comida: " + ex.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
-//        }
-//    }
-//
-//    private void mostrarDietaComida() {
-//        int idDietaComida = Integer.parseInt(vista.getTxtIdDietaComida().getText());
-//
-//        try {
-//            EntidadDieta_Comida dietaComida = dataDietaComida.obtenerDietaComidaPorId(idDietaComida);
-//
-//            if (dietaComida != null) {
-//                vista.getTxtIdDieta().setText(String.valueOf(dietaComida.getIdDieta()));
-//                vista.getTxtIdComida().setText(String.valueOf(dietaComida.getIdComida()));
-//                vista.getTxtPorcion().setText(String.valueOf(dietaComida.getPorcion()));
-//                vista.getCbHorario().setSelectedItem(dietaComida.getHorario());
-//            } else {
-//                vista.mostrarMensaje("La relación Dieta-Comida no se encontró en la base de datos.");
-//            }
-//        } catch (SQLException ex) {
-//            JOptionPane.showMessageDialog(vista, "Error al obtener relación Dieta-Comida: " + ex.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
-//        }
-//    }
-//
-//    private void listarDietasComida() {
-//        int idDieta = Integer.parseInt(vista.getTxtIdDieta().getText());
-//
-//        try {
-//            List<EntidadDieta_Comida> dietasComida = dataDietaComida.obtenerDietasComidaPorDieta(idDieta);
-//
-//            DefaultTableModel model = (DefaultTableModel) vista.getTbDietasComida().getModel();
-//            model.setRowCount(0);
-//
-//            for (EntidadDieta_Comida dietaComida : dietasComida) {
-//                Object[] rowData = {
-//                    dietaComida.getIdDietaComida(),
-//                    dietaComida.getIdDieta(),
-//                    dietaComida.getIdComida(),
-//                    dietaComida.getPorcion(),
-//                    dietaComida.getHorario()
-//                };
-//                model.addRow(rowData);
-//            }
-//        } catch (SQLException ex) {
-//            JOptionPane.showMessageDialog(vista, "Error al obtener las relaciones Dieta-Comida: " + ex.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
-//        }
-//    }
+    
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void focusGained(FocusEvent e) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void focusLost(FocusEvent e) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void valueChanged(ListSelectionEvent e) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
 }
 
